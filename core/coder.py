827c626373735992e0a3519eaea8f3ac1a49ef1a
schorio <schorioignace@gmail.com>
2024-03-26 13:02:36 +0100

    decodage

--- core/coder.py
+++ core/coder.py
@@ -6,3 +6,11 @@ with open("keylogger.py", "rb") as script_file:
     
 with open("encoder/encoded_keylogger.py", "wb") as encoded_file:
     encoded_file.write(encoded_script)
+    
+
+# Decode
+decoded_script = base64.b64decode(encoded_script)
+
+# Sauvegarde du code decodé
+with open("encoder/decoded_keylogger.py", "wb") as decoded_file:
+    decoded_file.write(decoded_script)
