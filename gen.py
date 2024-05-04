68c14cfdb7af74af876057b0f2dfe108672baa28
schorio <schorioignace@gmail.com>
2024-05-04 09:48:07 +0300

    creation du zone pour ajouter la longheur de mdp à generer

--- gen.py
+++ gen.py
@@ -7,4 +7,9 @@ import string
 root = ctk.CTk()
 root.title("Générateur de Mot de Passe")
 
+label_longueur = ctk.CTkLabel(root, text="Longueur du mot de passe :")
+label_longueur.grid(row=0, column=0, padx=5, pady=5)
+entry_longueur = ctk.CTkEntry(root)
+entry_longueur.grid(row=0, column=1, padx=5, pady=5)
+
 root.mainloop()
